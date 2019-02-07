b6028bbd427c658943c698e4137f3b5766c21bcd
jessgat <jessgat@hotmail.com>
2019-02-06 23:35:23 -0600

    poner y quitar pantallas

--- src/cipher.js
+++ src/cipher.js
@@ -45,6 +45,6 @@ window.cipher = {
     }
     return texto;
   }
-  // ... 
+  
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+ 
+      const firstScreen = document.getElementById("firstScreen");
+      const secondScreen = document.getElementById("secondScreen");
+      
+      firstScreen.style.display = 'block';
+      secondScreen.style.display = 'none';
+      
+
 function continueClick(){
 
     const userRank = document.getElementById("listRank").value; //toma el valor que el usuario escoge en la lista
     const userName = document.getElementById("policeName").value; // toma el nombre que da el usuario
-    
-         document.getElementById("rankWelcome").innerHTML = userRank; //el valor dado al rango, lo arroja en el HTML
-         document.getElementById("nameWelcome").innerHTML= userName; //el nombre del usuario lo da al HTML
+
+      document.getElementById("rankWelcome").innerHTML = userRank; //el valor dado al rango, lo arroja en el HTML
+      document.getElementById("nameWelcome").innerHTML= userName; //el nombre del usuario lo da al HTML
+
+      const firstScreen = document.getElementById("firstScreen");
+      const secondScreen = document.getElementById("secondScreen");
+      firstScreen.style.display = 'none';
+      secondScreen.style.display = 'block';
 }
 document.getElementById("continue").addEventListener("click",continueClick,false); //Acciona el botón de Continuar
 
@@ -15,6 +28,13 @@ function llamarencode(){
   let cifrado = cipher.encode(offset,word);
 
   document.getElementById("message2").value=cifrado;
+
+      const firstScreen = document.getElementById("firstScreen");
+      const secondScreen = document.getElementById("secondScreen");
+      const thirdScreen = document.getElementById("thirdScreen");
+      firstScreen.style.display = 'none';
+      secondScreen.style.display = 'none';
+      thirdScreen.style.display = 'block';
 }
   document.getElementById("encodeButton").addEventListener("click",llamarencode,false);
 
@@ -25,17 +45,42 @@ function llamardecode(){
   let cifrado = cipher.decode(offset,word);
   
   document.getElementById("message2").value=cifrado;
+      const firstScreen = document.getElementById("firstScreen");
+      const secondScreen = document.getElementById("secondScreen");
+      const thirdScreen = document.getElementById("thirdScreen");
+      firstScreen.style.display = 'none';
+      secondScreen.style.display = 'none';
+      thirdScreen.style.display = 'block';
 }
   document.getElementById("decodeButton").addEventListener("click",llamardecode,false);
 
 function yesClick(){
-  
-  //VUELVE A PANTALLA CIFRAR/DESCIFRAR
+  document.getElementById("message1").value="";
+  document.getElementById("message2").value="";
+
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
+  const thirdScreen = document.getElementById("thirdScreen");
+  firstScreen.style.display = 'none';
+  secondScreen.style.display = 'block';
+  thirdScreen.style.display = 'none';
   }
 document.getElementById("yesButton").addEventListener("click",yesClick,false);
 
 function noClick(){
-  document.clear();//LIMPIA PANTALLA
-  //VUELVE AL INICIO
+
+  pantallaInicio = document.getElementById("firstScreen").innerHTML;
+  pantallaInicio.scrolTop = 0; //VUELVE AL INICIO
+  document.getElementById("message1").value=""; //LIMPIA PANTALLA
+  document.getElementById("message2").value="";//LIMPIA PANTALLA
+  document.getElementById("listRank").value = ""; 
+  document.getElementById("policeName").value= "";
+
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
+  const thirdScreen = document.getElementById("thirdScreen");
+  firstScreen.style.display = 'block';
+  secondScreen.style.display = 'none';
+  thirdScreen.style.display = 'none';
  }
 document.getElementById("noButton").addEventListener("click",noClick,false);
