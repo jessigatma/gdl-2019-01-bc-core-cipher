ddd6a783fd4ff5919c41b23e067016cc683a910f
jessgat <jessgat@hotmail.com>
2019-02-06 23:44:52 -0600

    QUITANDO LO QUE NO ES ÚTIL EN EL CÓDIGO

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
- 
-      const firstScreen = document.getElementById("firstScreen");
-      const secondScreen = document.getElementById("secondScreen");
-      
-      firstScreen.style.display = 'block';
-      secondScreen.style.display = 'none';
-      
-
 function continueClick(){
 
     const userRank = document.getElementById("listRank").value; //toma el valor que el usuario escoge en la lista
@@ -14,8 +6,6 @@ function continueClick(){
       document.getElementById("rankWelcome").innerHTML = userRank; //el valor dado al rango, lo arroja en el HTML
       document.getElementById("nameWelcome").innerHTML= userName; //el nombre del usuario lo da al HTML
 
-      const firstScreen = document.getElementById("firstScreen");
-      const secondScreen = document.getElementById("secondScreen");
       firstScreen.style.display = 'none';
       secondScreen.style.display = 'block';
 }
@@ -23,64 +13,50 @@ document.getElementById("continue").addEventListener("click",continueClick,false
 
 
 function llamarencode(){
-  let offset = parseInt(document.getElementById("operativeCode").value);
-  let word  = document.getElementById("message1").value;
-  let cifrado = cipher.encode(offset,word);
+    let offset = parseInt(document.getElementById("operativeCode").value);
+    let word  = document.getElementById("message1").value;
+    let cifrado = cipher.encode(offset,word);
 
-  document.getElementById("message2").value=cifrado;
+       document.getElementById("message2").value=cifrado;
 
-      const firstScreen = document.getElementById("firstScreen");
-      const secondScreen = document.getElementById("secondScreen");
-      const thirdScreen = document.getElementById("thirdScreen");
-      firstScreen.style.display = 'none';
-      secondScreen.style.display = 'none';
-      thirdScreen.style.display = 'block';
+       firstScreen.style.display = 'none';
+       secondScreen.style.display = 'none';
+       thirdScreen.style.display = 'block';
 }
   document.getElementById("encodeButton").addEventListener("click",llamarencode,false);
 
 
 function llamardecode(){
-  let offset = parseInt(document.getElementById("operativeCode").value);
-  let word  = document.getElementById("message1").value;
-  let cifrado = cipher.decode(offset,word);
+     let offset = parseInt(document.getElementById("operativeCode").value);
+     let word  = document.getElementById("message1").value;
+     let cifrado = cipher.decode(offset,word);
   
-  document.getElementById("message2").value=cifrado;
-      const firstScreen = document.getElementById("firstScreen");
-      const secondScreen = document.getElementById("secondScreen");
-      const thirdScreen = document.getElementById("thirdScreen");
-      firstScreen.style.display = 'none';
-      secondScreen.style.display = 'none';
-      thirdScreen.style.display = 'block';
+        document.getElementById("message2").value=cifrado;
+
+        firstScreen.style.display = 'none';
+        secondScreen.style.display = 'none';
+        thirdScreen.style.display = 'block';
 }
   document.getElementById("decodeButton").addEventListener("click",llamardecode,false);
 
 function yesClick(){
-  document.getElementById("message1").value="";
-  document.getElementById("message2").value="";
+    document.getElementById("message1").value="";
+    document.getElementById("message2").value="";
 
-  const firstScreen = document.getElementById("firstScreen");
-  const secondScreen = document.getElementById("secondScreen");
-  const thirdScreen = document.getElementById("thirdScreen");
-  firstScreen.style.display = 'none';
-  secondScreen.style.display = 'block';
-  thirdScreen.style.display = 'none';
-  }
+    firstScreen.style.display = 'none';
+    secondScreen.style.display = 'block';
+    thirdScreen.style.display = 'none';
+}
 document.getElementById("yesButton").addEventListener("click",yesClick,false);
 
 function noClick(){
+    document.getElementById("message1").value=""; //LIMPIA PANTALLA
+    document.getElementById("message2").value="";//LIMPIA PANTALLA
+    document.getElementById("listRank").value = ""; 
+    document.getElementById("policeName").value= "";
 
-  pantallaInicio = document.getElementById("firstScreen").innerHTML;
-  pantallaInicio.scrolTop = 0; //VUELVE AL INICIO
-  document.getElementById("message1").value=""; //LIMPIA PANTALLA
-  document.getElementById("message2").value="";//LIMPIA PANTALLA
-  document.getElementById("listRank").value = ""; 
-  document.getElementById("policeName").value= "";
-
-  const firstScreen = document.getElementById("firstScreen");
-  const secondScreen = document.getElementById("secondScreen");
-  const thirdScreen = document.getElementById("thirdScreen");
-  firstScreen.style.display = 'block';
-  secondScreen.style.display = 'none';
-  thirdScreen.style.display = 'none';
- }
+    firstScreen.style.display = 'block';
+    secondScreen.style.display = 'none';
+    thirdScreen.style.display = 'none';
+}
 document.getElementById("noButton").addEventListener("click",noClick,false);
