9a764dd24c074696f822088db65e5abde33e5028
jessigatma <jessgat@hotmail.com>
2019-07-11 19:53:11 -0500

    se arreglaron problemas con el README

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
+# ¿Qué es Cipher?
+
 En el README.md, cuéntanos cómo pensaste en los usuarios y cuál fue tu proceso
 para definir el producto final a nivel de experiencia y de interfaz.
 
 Al pensar en un mensaje cifrado, lo primero que vino a mi mente fue la profesión de mi cuñado quien 
 es policía. Muchas veces hacen operativos y creí que sería muy útil para este sector. 
 
-# Principales usuarios del producto
+## Principales usuarios del producto
 
 El producto está pensado para uso de La Policía
 
-# Objetivos de los usuarios en relación con el producto
+## Objetivos de los usuarios en relación con el producto
 
-Pensando en alguna investigación donde se requiera un cuidado de información extremo, 
-los agentes pueden hacer uso de este producto para cifrar sus mensajes por medio de un 
-"código de operativo" que sólo van a conocer el remitente y el destinatario. El "código 
+Pensando en alguna investigación donde se requiera un cuidado de información extremo,
+los agentes pueden hacer uso de este producto para cifrar sus mensajes por medio de un
+"código de operativo" que sólo van a conocer el remitente y el destinatario. El "código
 de operativo" es el número de letras que se recorrerá nuestro cifrado César.
 
-# Problemas que resuelve
+### Problemas que resuelve
 
-Si en algún momento hay infiltrados dentro de la corporación, este producto sería muy
-útil para, incluso, descartar o señalar sospechosos. 
+Si en algún momento hay infiltrados dentro de la corporación,este producto sería muy útil para, incluso, descartar o señalar sospechosos.
--- src/index.js
+++ src/index.js
@@ -1,88 +1,87 @@
+function continueClick() {
 
+  const userRank = document.getElementById("listRank").value; //toma el valor que el usuario escoge en la lista
+  const userName = document.getElementById("policeName").value; // toma el nombre que da el usuario
 
-function continueClick(){
+  document.getElementById("rankWelcome").innerHTML = userRank; //el valor dado al rango, lo arroja en el HTML
+  document.getElementById("nameWelcome").innerHTML = userName; //el nombre del usuario lo da al HTML
 
-    const userRank = document.getElementById("listRank").value; //toma el valor que el usuario escoge en la lista
-    const userName = document.getElementById("policeName").value; // toma el nombre que da el usuario
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
 
-      document.getElementById("rankWelcome").innerHTML = userRank; //el valor dado al rango, lo arroja en el HTML
-      document.getElementById("nameWelcome").innerHTML= userName; //el nombre del usuario lo da al HTML
-      
-    const firstScreen = document.getElementById("firstScreen");
-    const secondScreen = document.getElementById("secondScreen");
-    
-       firstScreen.style.display = "none";
-       secondScreen.style.display = "block"; //me muestra la segunda pantalla
+  firstScreen.style.display = "none";
+  secondScreen.style.display = "block"; //me muestra la segunda pantalla
 }
-document.getElementById("continue").addEventListener("click",continueClick,false); //Acciona el botón de Continuar
+document.getElementById("continue").addEventListener("click", continueClick, false); //Acciona el botón de Continuar
 
 
-function llamarencode(){
-    let offset = parseInt(document.getElementById("operativeCode").value); // Convierte mi type:number en número
-    let word  = document.getElementById("message1").value.toUpperCase(); //obtiene la cadena de texto del textarea 
-    let cifrado = cipher.encode(offset,word); //llama al objeto cipher.js
+function llamarencode() {
+  let offset = parseInt(document.getElementById("operativeCode").value); // Convierte mi type:number en número
+  let word = document.getElementById("message1").value.toUpperCase(); //obtiene la cadena de texto del textarea 
+  let cifrado = cipher.encode(offset, word); //llama al objeto cipher.js
 
-       document.getElementById("message2").value=cifrado; //Me arroja el mensaje en el segundo recuadro, después de cifrarlo
+  document.getElementById("message2").value = cifrado; //Me arroja el mensaje en el segundo recuadro, después de cifrarlo
 
-    const firstScreen = document.getElementById("firstScreen");
-    const secondScreen = document.getElementById("secondScreen");
-    const thirdScreen = document.getElementById("thirdScreen");
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
+  const thirdScreen = document.getElementById("thirdScreen");
 
-       firstScreen.style.display = "none";
-       secondScreen.style.display = "none";
-       thirdScreen.style.display = "block"; //Me muestra sólo la tercer pantalla
-       document.getElementById("chooseCode").innerHTML = "cifrado"; //Si el usuario escoge cifrar, manda al HTML el mensaje: cifrado
-       document.getElementById("endecode").innerHTML = "Cifrar"; //Si el usuario escoge cifrar, pregunta al usuario si quiere volver a cifrar
-}
-  document.getElementById("encodeButton").addEventListener("click",llamarencode,false);  //Acciona el boton cifrar
- 
-
-function llamardecode(){
-     let offset = parseInt(document.getElementById("operativeCode").value);
-     let word = document.getElementById("message1").value.toUpperCase();
-     let cifrado = cipher.decode(offset,word);
+  firstScreen.style.display = "none";
+  secondScreen.style.display = "none";
+  thirdScreen.style.display = "block"; //Me muestra sólo la tercer pantalla
   
-        document.getElementById("message2").value=cifrado;
-    
-     const firstScreen = document.getElementById("firstScreen");
-     const secondScreen = document.getElementById("secondScreen");
-     const thirdScreen = document.getElementById("thirdScreen");
-
-        firstScreen.style.display = "none";
-        secondScreen.style.display = "none";
-        thirdScreen.style.display = "block";
-
-        document.getElementById("chooseCode").innerHTML = "descifrado";
-        document.getElementById("endecode").innerHTML = "Descifrar";
+  document.getElementById("chooseCode").innerHTML = "cifrado"; //Si el usuario escoge cifrar, manda al HTML el mensaje: cifrado
+  document.getElementById("endecode").innerHTML = "Cifrar"; //Si el usuario escoge cifrar, pregunta al usuario si quiere volver a cifrar
 }
-  document.getElementById("decodeButton").addEventListener("click",llamardecode,false); //Acciona el botón descifrar
+document.getElementById("encodeButton").addEventListener("click", llamarencode, false); //Acciona el boton cifrar
 
-function yesClick(){
-    document.getElementById("message1").value=""; //limpia los recuadros ya utilizados anteriormente
-    document.getElementById("message2").value="";
 
-    const firstScreen = document.getElementById("firstScreen");
-    const secondScreen = document.getElementById("secondScreen");
-    const thirdScreen = document.getElementById("thirdScreen");
+function llamardecode() {
+  let offset = parseInt(document.getElementById("operativeCode").value);
+  let word = document.getElementById("message1").value.toUpperCase();
+  let cifrado = cipher.decode(offset, word);
 
-      firstScreen.style.display = "none";
-      secondScreen.style.display = "block"; //Vuelve a la pantalla para volver a cifrar, pantalla 2
-      thirdScreen.style.display = "none";
+  document.getElementById("message2").value = cifrado;
+
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
+  const thirdScreen = document.getElementById("thirdScreen");
+
+  firstScreen.style.display = "none";
+  secondScreen.style.display = "none";
+  thirdScreen.style.display = "block";
+
+  document.getElementById("chooseCode").innerHTML = "descifrado";
+  document.getElementById("endecode").innerHTML = "Descifrar";
 }
-document.getElementById("yesButton").addEventListener("click",yesClick,false); //Acciona el botón sí
+document.getElementById("decodeButton").addEventListener("click", llamardecode, false); //Acciona el botón descifrar
 
-function noClick(){
-    document.getElementById("message1").value=""; //LIMPIA PANTALLA
-    document.getElementById("message2").value="";//LIMPIA PANTALLA
-    document.getElementById("listRank").value = ""; 
-    document.getElementById("policeName").value= "";
+function yesClick() {
+  document.getElementById("message1").value = ""; //limpia los recuadros ya utilizados anteriormente
+  document.getElementById("message2").value = "";
 
-    const firstScreen = document.getElementById("firstScreen");
-    const secondScreen = document.getElementById("secondScreen");
-    const thirdScreen = document.getElementById("thirdScreen");
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
+  const thirdScreen = document.getElementById("thirdScreen");
 
-      firstScreen.style.display = "block"; //Vuelve al inicio, muestra la pantalla 1
-      secondScreen.style.display = "none";
-      thirdScreen.style.display = "none";
+  firstScreen.style.display = "none";
+  secondScreen.style.display = "block"; //Vuelve a la pantalla para volver a cifrar, pantalla 2
+  thirdScreen.style.display = "none";
 }
-document.getElementById("noButton").addEventListener("click",noClick,false); //Acciona el botón no
+document.getElementById("yesButton").addEventListener("click", yesClick, false); //Acciona el botón sí
+
+function noClick() {
+  document.getElementById("message1").value = ""; //LIMPIA PANTALLA
+  document.getElementById("message2").value = ""; //LIMPIA PANTALLA
+  document.getElementById("listRank").value = "";
+  document.getElementById("policeName").value = "";
+
+  const firstScreen = document.getElementById("firstScreen");
+  const secondScreen = document.getElementById("secondScreen");
+  const thirdScreen = document.getElementById("thirdScreen");
+
+  firstScreen.style.display = "block"; //Vuelve al inicio, muestra la pantalla 1
+  secondScreen.style.display = "none";
+  thirdScreen.style.display = "none";
+}
+document.getElementById("noButton").addEventListener("click", noClick, false); //Acciona el botón no
